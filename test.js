4c7226a20386922a348146ab6317d888dbe3e748
Faïssal <faissal.hattou@gmail.com>
2017-12-08 11:00:52 +0100

    test 1 passé

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const chai = require('chai');
 const expect = require('chai').expect;
 const HelloWorld = require('./helloworld');
 
-describe('Hello World test', function() {
-  
-  it('Should return Hello World', function() {
+describe('Hello World test', function () {
+
+  it('Should return Hello World', function () {
     const helloWorld = new HelloWorld();
-    
+    return 'Hello World';
     expect(helloWorld.hey).to.equal('Hello World');
   });
-})
+});
