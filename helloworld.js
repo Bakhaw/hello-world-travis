2008619dd9304f98993278f4d908d3abbd6f43ab
Faïssal <faissal.hattou@gmail.com>
2017-12-08 11:04:00 +0100

    test 1 passé

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,6 @@
 function HelloWorld() {
   //Write your super difficult code here.
+  this.hey = 'Hello World';
 }
 
 module.exports = HelloWorld;
--- test.js
+++ test.js
@@ -6,7 +6,6 @@ describe('Hello World test', function () {
 
   it('Should return Hello World', function () {
     const helloWorld = new HelloWorld();
-    return 'Hello World';
     expect(helloWorld.hey).to.equal('Hello World');
   });
 });
